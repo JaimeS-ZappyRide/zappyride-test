2b321b4c5b9c5167829e956e5eed28fe9d7061da
nicsteinberg <nicole_steinberg@brown.edu>
2020-04-22 20:28:24 -0400

    show all WA stations, no lazy loading

--- zappyride-app/src/MapContainer.jsx
+++ zappyride-app/src/MapContainer.jsx
@@ -1,21 +1,23 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState} from 'react';
 import PropTypes from 'prop-types';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { Map, Marker, GoogleApiWrapper } from 'google-maps-react';
 
 export const MapContainer = ({google}) => {
-	const [results, setResults] = useState([]);
+	const [results, setResults] = useState({});
 
 	const callApi = () => {
-	    fetch('https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=h8pP3dk3ZPgI694vYTHSFmgNboSVlXdknQ4hjNep&location=Seattle+WA')
+	    fetch('https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=h8pP3dk3ZPgI694vYTHSFmgNboSVlXdknQ4hjNep&latitude=47.7511&longitude=-120.7401&radius=infinite&limit=all&state=WA')
 	        .then(res => res.json())
-	        .then(res => setResults(res));
+	        .then(res => setResults(res.fuel_stations));
     }
-
-    useEffect(callApi, []);
 
 	return (
 		<div>
-			<Map google={google}/>
+			<Map google={google} initialCenter={{lat: 47.6062, lng: -122.3321}} onReady={callApi}>
+				{results.length && results.map(stn => {
+					return <Marker key={stn.id} position={{lat: stn.latitude, lng: stn.longitude}}/>
+				})}
+			</Map>
 		</div>
 	);
 }
